430ece33bdd699738df17ac6c5369615a6d33c23
HyperEpsilon <6267244+HyperEpsilon@users.noreply.github.com>
2018-11-17 16:52:44 -0700

    Changed library to tweepy

--- Twitter-test.py
+++ Twitter-test.py
@@ -1,9 +1,18 @@
-import twitter
+import tweepy
 
-api = twitter.Api(consumer_key=['9DylZjpQIUOuRDVev7BRlShUS'],
-                  consumer_secret=['WvDaTcYz3c70xgsadKBdi9BrfjpQEiymiOrPsg2KNVnZppnQo6'],
-                  access_token_key=['1063915931066261504-FwDnh5XsrB0Hb0dVzsbfW0omgyMrKS'],
-                  access_token_secret=['BrNAzfPwQGzb1rAuJCBC2EiC6HvFEXpVFBh9AZEWrpoTy'])
+consumer_key='9DylZjpQIUOuRDVev7BRlShUS'
+consumer_secret='WvDaTcYz3c70xgsadKBdi9BrfjpQEiymiOrPsg2KNVnZppnQo6'
+access_token='1063915931066261504-FwDnh5XsrB0Hb0dVzsbfW0omgyMrKS'
+access_token_secret='BrNAzfPwQGzb1rAuJCBC2EiC6HvFEXpVFBh9AZEWrpoTy'
 
-statuses = api.GetUserTimeline(1063915931066261504)
-print([s.text for s in statuses])
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+## Bounding box coordingates: tweet.place.bounding_box.coordinates[0]
+
+tweets = api.home_timeline()
+#tweets = api.user_timeline(351978460, count=5)
+for tweet in tweets:
+    print(tweet.place)
